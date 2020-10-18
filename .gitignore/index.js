44a462974647abef02a176715005b6be75c19d17
mxopolokill <73064238+mxopolokill@users.noreply.github.com>
2020-10-18 14:49:47 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,18 @@
+const discord = require('discord.js')
+const client = new discord.Client()
+const config = require('./config.json')
+const handler = require("./handler")
+
+
+client.on('ready',()=>{
+    console.log('ok')
+    handler(client,config)
+    client.user.setActivity(" salut 🙂")
+    
+})
+
+
+
+
+
+client.login(config.token)
