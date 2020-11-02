5fdb14ccd8a899fe65b35809b3fe6022794918dc
mxopolokill <73064238+mxopolokill@users.noreply.github.com>
2020-11-02 02:30:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,7 @@ const discord = require('discord.js')
 const client = new discord.Client()
 const config = require('./config.json')
 const handler = require("./handler")
+const ytdl = require("ytdl-core");
 
 
 client.on('ready',()=>{
@@ -10,6 +11,32 @@ client.on('ready',()=>{
     client.user.setActivity(" salut 🙂")
     
 })
+
+client.on("message", message => {
+    if(message.content.startsWith(config.prefix + "play")){
+        if(message.member.voice.channel){
+            message.member.voice.channel.join().then(connection =>{
+                let args = message.content.split(" ");
+                
+                let dispatcher = connection.play(ytdl(args[1], {quality: "highestaudio"}));
+                dispatcher.on("finish", () =>{
+                    dispatcher.destroy();
+                    connection.disconnect();
+                });
+
+                dispatcher.on("error", err => {
+                    console.log("erreur de dispatcher : " + err);
+                });
+            }).catch(err =>{
+                message.reply("erreur lors de la connexion : " + err);
+            });
+        }
+        else{
+            message.reply("vous n'êtes pas connecté en vocal.");
+        }
+    }
+});
+
 
 
 
