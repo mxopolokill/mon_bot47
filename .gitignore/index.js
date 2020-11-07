4907b6493ff37e185a9e0f15a33ae0c353e040b7
mxopolokill <73064238+mxopolokill@users.noreply.github.com>
2020-11-07 21:45:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,45 +1,47 @@
-const discord = require('discord.js')
-const client = new discord.Client()
-const config = require('./config.json')
-const handler = require("./handler")
+const discord = require("discord.js");
+const client = new discord.Client();
+const config = require("./config.json");
+const handler = require("./handler");
 const ytdl = require("ytdl-core");
 
-
-client.on('ready',()=>{
-    console.log('ok')
-    handler(client,config)
-    client.user.setActivity(" salut 🙂")
-    
-})
-
-client.on("message", message => {
-    if(message.content.startsWith(config.prefix + "play")){
-        if(message.member.voice.channel){
-            message.member.voice.channel.join().then(connection =>{
-                let args = message.content.split(" ");
-                
-                let dispatcher = connection.play(ytdl(args[1], {quality: "highestaudio"}));
-                dispatcher.on("finish", () =>{
-                    dispatcher.destroy();
-                    connection.disconnect();
-                });
-
-                dispatcher.on("error", err => {
-                    console.log("erreur de dispatcher : " + err);
-                });
-            }).catch(err =>{
-                message.reply("erreur lors de la connexion : " + err);
-            });
-        }
-        else{
-            message.reply("vous n'êtes pas connecté en vocal.");
-        }
-    }
+client.on("ready", () => {
+  console.log("ok");
+  handler(client, config);
+  client.channels.cache.get("773275330521333821").send("redémarrage effectué");
+  client.channels.cache.get("774141788537356299").send("redémarrage effectué");
+  client.user.setActivity(" salut 🙂 ");
 });
 
+client.on("message", message => {
+  if (message.content.startsWith(config.prefix + "song")) {
+    if (message.member.voice.channel) {
+      message.member.voice.channel
+        .join()
+        .then(connection => {
+          let args = message.content.split(" ");
+          message.reply("L'audio à démarré");
 
+          let dispatcher = connection.play(
+            ytdl(args[1], { quality: "highestaudio" })
+          );
 
+          dispatcher.on("finish", () => {
+            dispatcher.destroy();
+            connection.disconnect();
+            message.reply("L'audio est terminé");
+          });
 
+          dispatcher.on("error", err => {
+            console.log("erreur de dispatcher : " + err);
+          });
+        })
+        .catch(err => {
+          message.reply("erreur lors de la connexion : " + err);
+        });
+    } else {
+      message.reply("vous n'êtes pas connecté en vocal.");
+    }
+  }
+});
 
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
