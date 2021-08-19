c20bbb85062b51cab9e226a24cca4306fb3f6c37
mxopolokill <73064238+mxopolokill@users.noreply.github.com>
2021-08-19 06:03:04 +0200

    Update 2.1.5
    mise a jours terminer

--- handler.js
+++ handler.js
@@ -1,21 +1,16 @@
-module.exports = (client, config) => {
-  client.on("message", (m) => {
-    if (m.content[0] == config.prefix) {
-      m.cmd = m.content.replace(config.prefix, "").split(config.separator);
-      let command_Role = config.commands.find(
-        (c) => c.command_name == m.cmd[0]
-      );
-      if (command_Role) {
-        m.member.roles
-          .add(command_Role.role_id)
-          .then((mf) => {
-            m.channel.send("Votre role a été ajouté");
-          })
-          .catch((err) => {
-            m.channel.send("Impossible d'ajouté le role (" + err.message + ")");
-          });
-      } else {
-        /*Autre commande perso
+module.exports = (client,config) =>{
+    client.on('message',m=>{
+        if(m.content[0]==config.prefix){
+            m.cmd = m.content.replace(config.prefix,"").split(config.separator)
+            let command_Role =  config.commands.find(c=> c.command_name == m.cmd[0])
+            if(command_Role){
+                m.member.roles.add(command_Role.role_id).then(mf=>{
+                    m.channel.send("Votre role a été ajouté")
+                }).catch(err=>{
+                    m.channel.send("Impossible d'ajouté le role ("+err.message+")")
+                })
+            }else{
+                /*Autre commande perso
                 if(m.cmd[0] == "!NOM DE LA COMMANDE"){
     
                 }else if(m.cmd[0] == "!NOM DE LA COMMANDE"){
@@ -23,7 +18,7 @@ module.exports = (client, config) => {
                 }else{
                     //commande inconnue
                 }*/
-      }
-    }
-  });
-};
+            }
+        }
+    })
+}
